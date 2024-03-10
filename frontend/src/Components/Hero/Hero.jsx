bfb900deda455e212f487ddba074cf9cff52f515
yadavnikhil17102004 <yadavnikhil17102004@gmail.com>
2024-03-10 15:17:08 +0530

    Hero page added

---
+++ frontend/src/Components/Hero/Hero.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import './Hero.css'
+import hand_icon from '../Assets/hand_icon.png'
+import arrow from '../Assets/arrow.png'
+import hero_image from '../Assets/hero_image2.png'
+const Hero = () => {
+  return (
+    <div className='hero' >
+        <div className="hero-left">
+            <h2>NEW ARRIVALS ONLY</h2>
+            <div>
+                <div className="hero-hand-icon">
+                    <p>new</p>
+                    <img src={hand_icon} alt="hand_icon" />
+                </div>
+                <p>Pets</p>
+                <p>For Everyone</p>
+            </div>
+            <div className="hero-latest-btn">
+                <div>Newly added</div>
+                <img src={arrow} alt="arrow" />
+            </div>
+        </div>
+        <div className="hero-right">
+            <img src={hero_image} alt='hero_image' />
+        </div>
+    </div>
+  )
+}
+
+export default Hero
--- frontend/src/Pages/Shop.jsx
+++ frontend/src/Pages/Shop.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Hero from '../Components/Hero/Hero'
 
 const shop = () => {
   return (
-    <div>shop</div>
+    <div>
+      <Hero/>
+    </div>
   )
 }
 
